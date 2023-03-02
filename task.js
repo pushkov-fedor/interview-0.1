33cc68ceff4c02d4f4f79035fe24aa0cda5a2d16
Fedor Pushkov <fedor.pushkov@bimeister.com>
2023-03-02 18:57:01 +0700

    reset task.js

--- task.js
+++ task.js
@@ -12,47 +12,5 @@
  */
 
 export function getWeirdNumberIndex(input) {
-  if (!input || typeof input !== "string") {
-    return undefined;
-  }
-
-  let array = input.split(" ");
-
-  if (array.length <= 1) {
-    return undefined;
-  }
-
-  let oddCounter = 0;
-  let evenCounter = 0;
-
-  let oddIndex = -1;
-  let evenIndex = -1;
-
-  for (let i = 0; i < array.length; i++) {
-    if (typeof parseInt(array[i]) !== "number") {
-      return undefined;
-    }
-
-    if (array[i] % 2 === 0) {
-      evenIndex = i;
-      evenCounter++;
-    } else {
-      oddIndex = i;
-      oddCounter++;
-    }
-  }
-
-  if (evenCounter === oddCounter || evenCounter === 0 || oddCounter === 0) {
-    return undefined;
-  }
-
-  if (oddCounter === 1) {
-    return oddIndex;
-  }
-
-  if (evenCounter === 1) {
-    return evenIndex;
-  }
-
-  return undefined;
+  throw new Error("Not implemented");
 }
